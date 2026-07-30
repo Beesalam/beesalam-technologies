32e8088b0a582ae565b8262263e1c2aba76ed7df
Beesalam <abdulsalambabatunde305@gmail.com>
2026-07-30 03:16:28 +0100

    refactor: move Firebase config to environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK-e5d5VyxwBI-nv51Vc86wwqb1aha4fY",
-  authDomain: "beesalam-technologies.firebaseapp.com",
-  projectId: "beesalam-technologies",
-  storageBucket: "beesalam-technologies.firebasestorage.app",
-  messagingSenderId: "286978965294",
-  appId: "1:286978965294:web:66deb909faeac691acc4bf",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
 
 export default app;
